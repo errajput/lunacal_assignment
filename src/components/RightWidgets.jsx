28c70024a46397093e4a43e9eaa0171cf40b8016
disha rajput <dishacsevira@gmail.com>
2025-10-26 19:48:58 +0530

    add some style

--- src/components/RightWidgets.jsx
+++ src/components/RightWidgets.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function RightWidgets({ children, className = "" }) {
   return (
     <div
-      className={`relative rounded-2xl p-6 border border-white/5 backdrop-blur-md bg-linear-to-b from-[rgba(255,255,255,0.02)] to-[rgba(0,0,0,0.15)] ${className}`}
+      className={`relative rounded-2xl border border-white/5 backdrop-blur-md bg-linear-to-b from-[rgba(255,255,255,0.02)] to-[rgba(0,0,0,0.15)] ${className}`}
       style={{
         boxShadow:
           "0 18px 40px rgba(2,6,10,0.6), inset 0 1px 0 rgba(255,255,255,0.02)",
--- src/components/TabsWidget.jsx
+++ src/components/TabsWidget.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function TabsWidget({ tabs = [], active, onChange }) {
   return (
-    <div className="bg-[#0f0f0f] rounded-xl flex items-center gap-3 flex-wrap p-1.5">
+    <div className="bg-[#0f0f0f] rounded-[18px] flex items-center gap-1.5 flex-wrap p-1.5 mr-[53px]">
       {tabs.map((t) => {
         const isActive = active === t;
         return (
@@ -12,15 +12,23 @@ export default function TabsWidget({ tabs = [], active, onChange }) {
             key={t}
             onClick={() => onChange(t)}
             aria-pressed={isActive}
-            className={`relative px-5 py-2 rounded-lg text-sm font-medium select-none transition-all duration-300
+            className={`
+              
+              cursor-pointer relative w-[32%] px-5 py-2 rounded-xl text-sm font-medium select-none transition-all duration-300
               ${
                 isActive
-                  ? "bg-[#303030]/90 text-white shadow-[0_6px_20px_-8px_rgba(0,0,0,0.8)] scale-[1.02]"
-                  : "bg-white/5 text-gray-400 hover:bg-white/10 hover:text-white"
+                  ? "bg-[#303030]/90  text-white shadow-[0_6px_20px_-8px_rgba(0,0,0,0.8)] scale-[1.02]"
+                  : " "
               }
-              focus:outline-none focus:ring-2 focus:ring-gray-500/40`}
+              group overflow-hidden 
+              `}
           >
-            {t}
+            {!isActive && (
+              <span className="absolute inset-0 -translate-x-full group-hover:translate-x-0 transform transition-transform duration-400 ease-in-out bg-white/4 z-0"></span>
+            )}
+            <span className="relative z-10  transition-colors duration-300">
+              {t}
+            </span>
           </button>
         );
       })}
