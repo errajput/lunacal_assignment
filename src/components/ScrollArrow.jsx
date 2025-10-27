f531e86807a03c089198a360ce9dd0bc92241f26
disha rajput <dishacsevira@gmail.com>
2025-10-27 16:18:51 +0530

    add img and icon

--- src/components/ScrollArrow.jsx
+++ src/components/ScrollArrow.jsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useState } from "react";
+import { GoArrowLeft, GoArrowRight, GoArrowUpLeft } from "react-icons/go";
 
 export default function ScrollArrows({
   onLeftClick,
@@ -25,13 +26,14 @@ export default function ScrollArrows({
         className={`w-10 h-10 flex items-center justify-center rounded-full transition-all cursor-pointer duration-500 ease-in-out
           ${
             !canScrollLeft
-              ? "bg-[#1e2630] shadow-inner shadow-black/60 "
+              ? "bg-[#6c88a8] text-white shadow-[-4px_-8px_10px_rgba(107,144,195,0.35),4px_4px_10px_rgba(0,0,0,0.8)] "
               : activeButton === "left"
-              ? "bg-[#6c88a8]  shadow-[-4px_-8px_10px_rgba(107,144,195,0.35),4px_4px_10px_rgba(0,0,0,0.8)]"
-              : "bg-[#0f1112]/40 hover:bg-[#0f1112]/70 hover:shadow-[-6px_-6px_14px_rgba(107,144,195,0.5),6px_6px_14px_rgba(0,0,0,0.9)]"
+              ? "bg-[#1e2630]  shadow-inner shadow-black/60 "
+              : "bg-[#0f1112]/40 hover:bg-[#0f1112]/70 shadow-[-4px_-8px_10px_rgba(107,144,195,0.10),4px_4px_10px_rgba(0,0,0,0.8)]"
           }`}
       >
-        <Image src="/Group-29.png" alt="left-arrow" width={13} height={10} />
+        {/* <Image src="/Group-28.svg" alt="left-arrow" width={13} height={10} /> */}
+        <GoArrowLeft alt="left-arrow" width={13} height={10} />
       </button>
 
       {/* Right Arrow */}
@@ -41,13 +43,14 @@ export default function ScrollArrows({
         className={`w-10 h-10 flex items-center justify-center rounded-full transition-all cursor-pointer duration-500 ease-in-out
           ${
             !canScrollRight
-              ? "bg-[#1e2630] shadow-inner shadow-black/60"
+              ? " bg-[#6c88a8] text-white shadow-[-4px_-8px_10px_rgba(107,144,195,0.35),4px_4px_10px_rgba(0,0,0,0.8)]"
               : activeButton === "right"
-              ? "bg-[#6c88a8]  shadow-[-4px_-8px_10px_rgba(107,144,195,0.35),4px_4px_10px_rgba(0,0,0,0.8)]"
-              : "bg-[#0f1112]/40 hover:bg-[#0f1112]/70 hover:shadow-[-6px_-6px_14px_rgba(107,144,195,0.5),6px_6px_14px_rgba(0,0,0,0.9)]"
+              ? "bg-[#1e2630] shadow-inner shadow-black/60"
+              : "bg-[#0f1112]/40 hover:bg-[#0f1112]/70 shadow-[-4px_-8px_10px_rgba(107,144,195,0.10),4px_4px_10px_rgba(0,0,0,0.8)]"
           }`}
       >
-        <Image src="/Group-28.png" alt="right-arrow" width={13} height={10} />
+        <GoArrowRight alt="left-arrow" width={13} height={10} />
+        {/* <Image src="/Group-28.png" alt="right-arrow" width={13} height={10} /> */}
       </button>
     </div>
   );
